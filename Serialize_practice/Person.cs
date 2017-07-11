9c265c769220e5e9407a495d8f6f86f58084eac4
bozsob <bozso.beatrix@gmail.com>
2017-07-11 14:09:31 +0200

    Add project files.

---
+++ Serialize_practice/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialize_practice
+{
+    [Serializable]
+    public class Person
+    {
+        private string name;
+        private DateTime birthDate;
+        public enum Genders { Male, Female };
+
+        // constructors
+        public Person() { }
+
+        public Person(string name, DateTime birthDate)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+        }
+
+        // Properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Name: {0}, Birth date: {1}", this.name, this.birthDate);
+        }
+    }
+}
