337f0770c748c0c3e2d372e4116710da33583c29
bozsob <bozso.beatrix@gmail.com>
2017-07-11 15:35:04 +0200

    modified: Person class to optimize the deserialization age is claculated now

--- Serialize_practice/Person.cs
+++ Serialize_practice/Person.cs
@@ -8,10 +8,11 @@ using System.Runtime.Serialization;
 namespace Serialize_practice
 {
     [Serializable]
-    public class Person
+    public class Person : IDeserializationCallback
     {
         private string name;
         private DateTime birthDate;
+        [NonSerialized] public int age;
         public enum Genders { Male, Female };
 
         public Person() { }
@@ -34,9 +35,28 @@ namespace Serialize_practice
             set { birthDate = value; }
         }
 
+
         public override string ToString()
         {
-            return String.Format("Name: {0}, Birth date: {1}", this.name, this.birthDate);
+            return String.Format("Name: {0}, Birth date: {1}, Age: {2}", 
+                this.name, this.birthDate, CalculateAge(this));
+        }
+
+        void IDeserializationCallback.OnDeserialization(Object sender)
+        {
+            // after deserialization calculate age by the actual date
+            CalculateAge(this);
+        }
+
+        static int CalculateAge(Person p)
+        {
+            int age = DateTime.Now.Year - p.birthDate.Year;
+
+            if (DateTime.Now.Month < p.birthDate.Month || 
+                (DateTime.Now.Month == p.birthDate.Month && DateTime.Now.Day < p.birthDate.Day))
+                age = age -1;
+
+            return age;
         }
     }
 }
